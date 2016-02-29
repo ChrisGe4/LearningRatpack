b24dc5583cc0dad86e6f2ad27ab2ac979d48e5c3
Chris Ge <chris_ge@gallup.com>
2016-02-29 15:37:35 -0600

    add spring social

--- src/main/java/learningspringboot/IssueManagerApplication.java
+++ src/main/java/learningspringboot/IssueManagerApplication.java
@@ -3,10 +3,11 @@ package learningspringboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Indicates a configuration class that declares one or more @Bean methods and also triggers auto-configuration and component scanning. This is a convenience annotation that is equivalent to declaring @Configuration, @EnableAutoConfiguration and @ComponentScan.
 @SpringBootApplication
 public class IssueManagerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(IssueManagerApplication.class, args);
-	}
+    public static void main (String[] args) {
+        SpringApplication.run(IssueManagerApplication.class, args);
+    }
 }
