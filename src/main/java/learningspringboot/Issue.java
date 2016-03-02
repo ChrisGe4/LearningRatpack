ed8d46e7ea4211863677c9e6a55f8008bd065e89
Chris Ge <chris_ge@gallup.com>
2016-03-02 16:46:17 -0600

    run the issue manager app

---
+++ src/main/java/learningspringboot/Issue.java
@@ -0,0 +1,25 @@
+package learningspringboot;
+
+import org.springframework.social.github.api.GitHubIssue;
+
+/**
+ * @author chris_ge
+ */
+public class Issue {
+
+    private String repo;
+    private GitHubIssue gitHubIssue;
+
+    public Issue (String repo, GitHubIssue gitHubIssue) {
+        this.repo = repo;
+        this.gitHubIssue = gitHubIssue;
+    }
+
+    public String getRepo () {
+        return repo;
+    }
+
+    public GitHubIssue getGitHubIssue () {
+        return gitHubIssue;
+    }
+}
---
+++ src/main/java/learningspringboot/IssueController.java
@@ -0,0 +1,27 @@
+package learningspringboot;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author chris_ge
+ */
+@Controller(value = "Issue")
+public class IssueController {
+
+    private IssueManager issueManager;
+
+    @Autowired
+    public IssueController (IssueManager issueManager) {
+        this.issueManager = issueManager;
+    }
+
+    @RequestMapping(value = "/")
+    public String index (Model model) {
+        model.addAttribute("issues", issueManager.findOpenIssues());
+        return "index";
+
+    }
+}
